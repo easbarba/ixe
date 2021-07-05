2fef99d02210c85c8ffdd4a5bfd2f65b38d02f2b
easbarbosa <easbarbosa@tutanota.me>
2021-07-04 21:46:50 -0300

    require all files

--- lib/ixe.rb
+++ lib/ixe.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
-require_relative "ixe/version"
+require_relative 'ixe/version'
 
-module Ixe
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# * Services
+require_relative 'ixe/services/utils'
+require_relative 'ixe/services/folders'
+
+# * Get
+require_relative 'ixe/get/getting'
+require_relative 'ixe/get/info'
+require_relative 'ixe/get/main'
+require_relative 'ixe/get/parser'
+require_relative 'ixe/get/grabbers/youtube_dl'
+
+# * Play
+require_relative 'ixe/play/main'
+require_relative 'ixe/play/players/mpv'
